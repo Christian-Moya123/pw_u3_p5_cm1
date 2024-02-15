8690f6142d979fd20e921acfbcb4d9c6d12e31d0
Mona <cmoyacharro@gamil.com>
2024-02-14 20:54:36 -0500

    taller 34 cliente vue

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -37,6 +38,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 //SERVICIO -> Controller: Clase controller
 @RestController
 @RequestMapping(path="/estudiantes")
+@CrossOrigin
 public class EstudianteControllerRestFull {
 
 	@Autowired
